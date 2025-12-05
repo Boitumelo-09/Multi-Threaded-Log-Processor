8c487bda6cc55ce68a5efeab9bf1b51617711182
boitumelo-09 <belmiredub4@gmail.com>
2025-12-05 03:33:11 +0200

    UPDATE : UPDATED THE CLASS

--- Log.h
+++ Log.h
@@ -2,7 +2,8 @@
 #include <string>
 #include <iostream>
 #include <mutex>
-#include <queue>
+#include <ctime>
+#include <vector>
 #include <condition_variable>
 
 std::mutex _MUTEXLOCK;
@@ -11,13 +12,16 @@ std::condition_variable _CONDITION_VARIABLE;
 
 class Log
 {
-	std::queue<std::string> logQueue;
+	std::vector<std::string> _IDENTIFIERS;
+	std::vector<std::string> _MESSAGES;
 public:
+	
 	Log();
-	~Log();
+	
 
 	std::string generateLog();
-		
+	std::vector<std::string> getIdentifiers();
+	std::vector<std::string> getMessages();
 };
 
 //this class will have a que and generate logs
