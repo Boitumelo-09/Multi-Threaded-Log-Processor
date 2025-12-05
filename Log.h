e20d243478b5161a825bd4e088f66e18ee2384b2
boitumelo-09 <belmiredub4@gmail.com>
2025-12-05 02:39:25 +0200

    UPDATE : Added methods for the queue class, readying for implementation

--- Log.h
+++ Log.h
@@ -1,9 +1,23 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <condition_variable>
+
+std::mutex _MUTEXLOCK;
+std::condition_variable _CONDITION_VARIABLE;
+
 
 class Log
 {
-
+	std::queue<std::string> logQueue;
 public:
+	Log();
+	~Log();
+
+	std::string generateLog();
+		
 };
 
 //this class will have a que and generate logs
