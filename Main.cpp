8ab65d2914e2e09896850607d13ecb20c06b3a27
boitumelo-09 <belmiredub4@gmail.com>
2025-12-05 22:19:16 +0200

    UPDATE : IMPLEMENTING FUNTIONS TO GENERATE LOGS RANDOMY IN THE MAIN THREAD

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
+#include <ctime>
+#include <vector>
+#include <queue>
 
 
-
+std::string generateLog(std::vector<std::string>& head, std::vector<std::string>& messages);
 int main(void) {
-
-	std::thread _thread01([](int num) {std::cout << "Inside thread : " << num; }, 1);
-	_thread01.join();
+    std::vector<std::string> types = { "INFO", "WARNING", "ERROR" };
+    std::vector<std::string> messages = {
+        "Temperature reading normal",
+        "Memory usage high",
+        "Sensor disconnected",
+        "Battery low",
+        "Voltage spike detected"
+    };
+    std::string newLog = generateLog(types, messages);
 
 	return 0;
+}
+
+std::string generateLog(std::vector<std::string>& head, std::vector<std::string>& messages) {
+
+
 }
