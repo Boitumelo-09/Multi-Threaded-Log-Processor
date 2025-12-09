839d04aa660fb6ebe0560d1ed803b87813e9382c
boitumelo-09 <belmiredub4@gmail.com>
2025-12-10 00:25:23 +0200

    UPDATE : ADDED A COUNTER CLASS TO KEEP TRACK OF SYSTEM FUNTIONALITY

--- Counter.cpp
+++ Counter.cpp
@@ -1,4 +1,5 @@
 #include "Counter.h"
 
+
 void Counter::increment() { counter.fetch_add(1, std::memory_order_relaxed); }
 size_t Counter::getCounter() { return counter.load(std::memory_order_relaxed); }
