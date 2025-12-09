67fd6b5d75f13a7eac045375960e3affaee30773
boitumelo-09 <belmiredub4@gmail.com>
2025-12-10 00:15:02 +0200

    UPDATE : CREATED A CLASS FOR THE ATOMIC COUNTER

--- Counter.cpp
+++ Counter.cpp
@@ -1,1 +1,4 @@
 #include "Counter.h"
+
+void Counter::increment() { counter.fetch_add(1, std::memory_order_relaxed); }
+size_t Counter::getCounter() { return counter.load(std::memory_order_relaxed); }
--- Counter.h
+++ Counter.h
@@ -1,11 +1,13 @@
 #pragma once
 #include <atomic>
+#include "utility.h"
 
 
 class Counter
 {
-
+	std::atomic<size_t> counter{ 0 };
 public:
-
+	void increment();
+	size_t getCounter();
 };
 
