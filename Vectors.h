98a290f300f1dd769da18bec56754c179d525e26
boitumelo-09 <belmiredub4@gmail.com>
2025-12-10 04:49:32 +0200

    FIX : Fixed LNK2001 Error =>unresolved external symbol on Vector initializations

--- Vectors.h
+++ Vectors.h
@@ -4,7 +4,7 @@
 class Vectors
 {
 	
-	static const std::vector<text_t> logIdentifiers;
+	static const  std::vector<text_t> logIdentifiers;
 	static const  std::vector<text_t> logMessages;
 	std::mutex _MUTEX;
 	std::condition_variable _CONDVAR;
