534bbcea6277c4e2ff8335285bfad9c870036878
boitumelo-09 <belmiredub4@gmail.com>
2025-12-10 04:51:40 +0200

    UPDATE : implemented the run() metho that will run our facade class

--- System.cpp
+++ System.cpp
@@ -1,3 +1,14 @@
 #include "System.h"
 
+void System::run() {
+	srand(static_cast<unsigned int>(time(NULL)));
+
+	
+	print(horizontalPadding());
+	for (auto i : { 'S','y','s','t','e','m',' ','i','s',' ','R','u','n','n','i','n','g' }) {
+		std::cout << i;
+		std::this_thread::sleep_for(50ms);
+	}
+	pressToContinue();
+}
 
--- System.h
+++ System.h
@@ -1,6 +1,11 @@
 #pragma once
+#include "Vectors.h"
+#include "LogQueue.h"
+
 class System
 {
 
+public:
+	void run();
 };
 
