aee11ddf58ddfba128050726b19640d8bad0cde4
boitumelo-09 <belmiredub4@gmail.com>
2025-12-10 16:53:21 +0200

    UPDATE : inplemented the design flow using pointer objects...

--- System.cpp
+++ System.cpp
@@ -1,14 +1,37 @@
 #include "System.h"
 
+Shutdown* _shutdown = new Shutdown();
+Counter* _counter = new Counter();
+Vectors* vectorObjectContainer = new Vectors();
+LogQueue queueObjectContainer(*_shutdown, *_counter);
+
+
 void System::run() {
 	srand(static_cast<unsigned int>(time(NULL)));
 
-	
-	print(horizontalPadding());
-	for (auto i : { 'S','y','s','t','e','m',' ','i','s',' ','R','u','n','n','i','n','g' }) {
-		std::cout << i;
+	for (auto i : { 'S','y','s','t','e','m',' ','i','s',' ','R','u','n','n','i','n','g','.','.','.'}) {
+		
+		printChar(i);
 		std::this_thread::sleep_for(50ms);
 	}
 	pressToContinue();
+
+	ATHREAD_t threadS001([&] {
+		 while (!_shutdown->getShutdownState()) {
+			text_t newLog = vectorObjectContainer->generateLog();
+			queueObjectContainer.pushGeneratedLogs(newLog);
+			std::this_thread::sleep_for(1000ms);
+		}
+		});
+	ATHREAD_t threadS002([&] {
+		
+		while (!_shutdown->getShutdownState())
+		{
+			queueObjectContainer.popGeneratedLogs();
+		}
+		});
+
+	threadS001.join();
+	threadS002.join();
 }
 
--- System.h
+++ System.h
@@ -1,11 +1,15 @@
 #pragma once
+
 #include "Vectors.h"
 #include "LogQueue.h"
+#include "Shutdown.h"
+#include "Counter.h"
 
 class System
 {
 
 public:
+
 	void run();
 };
 
