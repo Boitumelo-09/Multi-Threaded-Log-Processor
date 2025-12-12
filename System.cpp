4c2dfa8b9790eda606349d5f22107bf6f8f722d3
boitumelo-09 <belmiredub4@gmail.com>
2025-12-12 23:49:30 +0200

    VOID : commented out the third thread executing the method to sow the number of logs generated

--- System.cpp
+++ System.cpp
@@ -4,17 +4,22 @@ Shutdown* _shutdown = new Shutdown();
 Counter* _counter = new Counter();
 Vectors* vectorObjectContainer = new Vectors();
 LogQueue queueObjectContainer(*_shutdown, *_counter);
-
+std::mutex _mu;
+std::condition_variable _cv;
 
 void  System::run() {
 	srand(static_cast<unsigned int>(time(NULL)));
+	{
+		std::unique_lock<std::mutex> _rLock(_mu);
+		for (auto i : { 'S','y','s','t','e','m',' ','i','s',' ','R','u','n','n','i','n','g','.','.','.' }) {
 
-	for (auto i : { 'S','y','s','t','e','m',' ','i','s',' ','R','u','n','n','i','n','g','.','.','.'}) {
-		
-		printChar(i);
-		std::this_thread::sleep_for(50ms);
+			printChar(i);
+			std::this_thread::sleep_for(50ms);
+		}
+		pressToContinue();
+		_cv.notify_all();
 	}
-	pressToContinue();
+
 
 	ATHREAD_t threadS001([&] {
 		 while (!_shutdown->getShutdownState()) {
@@ -25,24 +30,30 @@ void  System::run() {
 		});
 	ATHREAD_t threadS002([&] {
 		verticalPadding();
-		text_t headerMessage = (horizontalPadding() + "Registered Logs...");
-		printMessage(headerMessage,50);
+		
+		{
+			std::unique_lock<std::mutex> _rLock(_mu);
+			text_t headerMessage = (horizontalPadding() + "Registered Logs...");
+			printMessage(headerMessage, 5);
+			_cv.notify_all();
+		}
+
 		while (!_shutdown->getShutdownState())
 		{
 			queueObjectContainer.popGeneratedLogs();
 		}
 		pressToContinue();
 		});
-	ATHREAD_t threadS003([&] {
+	/*ATHREAD_t threadS003([&] {
 		
 		
 			queueObjectContainer.showLogRegistration();
 		
-		});
+		});*/
 	
 	threadS001.join();
 	threadS002.join();
-	threadS003.join();
+	/*threadS003.join();*/
 	
 	
 }
