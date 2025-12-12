c74eb8cbc75676de3c2f3986e3a091b433c67083
boitumelo-09 <belmiredub4@gmail.com>
2025-12-12 23:28:38 +0200

    FIX : fixed the data racing issue within the printing

--- System.cpp
+++ System.cpp
@@ -6,7 +6,7 @@ Vectors* vectorObjectContainer = new Vectors();
 LogQueue queueObjectContainer(*_shutdown, *_counter);
 
 
-void System::run() {
+void  System::run() {
 	srand(static_cast<unsigned int>(time(NULL)));
 
 	for (auto i : { 'S','y','s','t','e','m',' ','i','s',' ','R','u','n','n','i','n','g','.','.','.'}) {
@@ -24,14 +24,26 @@ void System::run() {
 		}
 		});
 	ATHREAD_t threadS002([&] {
-		
+		verticalPadding();
+		text_t headerMessage = (horizontalPadding() + "Registered Logs...");
+		printMessage(headerMessage,50);
 		while (!_shutdown->getShutdownState())
 		{
 			queueObjectContainer.popGeneratedLogs();
 		}
+		pressToContinue();
 		});
-
+	ATHREAD_t threadS003([&] {
+		
+		
+			queueObjectContainer.showLogRegistration();
+		
+		});
+	
 	threadS001.join();
 	threadS002.join();
+	threadS003.join();
+	
+	
 }
 
